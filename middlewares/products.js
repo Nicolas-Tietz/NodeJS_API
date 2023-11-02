373af0fc9937c59c2b1854e296020380a9f61ef3
Nicolas Tietz <nicolastietz48@gmail.com>
2023-11-02 12:32:04 +0100

    Changed field 'name' with 'productName'

--- middlewares/products.js
+++ middlewares/products.js
@@ -7,13 +7,13 @@ async function listProducts(req,res){
 
 async function addProduct(req,res){
     try{
-        if (Object.keys(req.body).length != 1 || (Object.keys(req.body)[0] != 'name') ) return res.status(400).send('Product requires one field: name')
-        const productName = req.body.name
+        if (Object.keys(req.body).length != 1 || (Object.keys(req.body)[0] != 'productName') ) return res.status(400).send('Product requires one field: productName')
+        const productName = req.body.productName
         
         
         
         const result = await Product.create({
-            "name": productName
+            "productName": productName
             
         })
         if (result) res.json(result)
@@ -36,7 +36,7 @@ async function deleteProduct(req,res){
         if (!product) return res.send('Product doesnt exists')
         const result = await Product.deleteOne({_id: req.params.id}).exec()
         if (result.acknowledged == true){
-            res.status(200).send(`Product ${product.name} Deleted Successfully`)
+            res.status(200).send(`Product ${product.productName} Deleted Successfully`)
 
         }else{
             res.status(400).send('Something went wrong during the product deletion')
@@ -48,15 +48,16 @@ async function deleteProduct(req,res){
 
 async function updateProduct(req,res){
     try{
-        const productName = req.body.name
+        const productName = req.body.productName
         let preUpdateProduct;
+        let ordersUpdatedString = ''
 
         if (req.body.length == 0 || req.body.length >= 2 ) return res.send('Operation cancelled. Body has 1 field.')
         
         
 
         for (const elem in req.body){
-            if (elem != 'name'){
+            if (elem != 'productName'){
                 return res.status(400).send(
                     `There is no field called ${elem}.<br/>
                     Fields are: product`
@@ -65,21 +66,23 @@ async function updateProduct(req,res){
         }
 
         preUpdateProduct = await Product.findOne({_id: req.params.id})
-
-        const result = await Product.findOneAndUpdate({_id: req.params.id},{$set: {name:productName} }, {new: true})
-            
+        if (productName == preUpdateProduct.productName) return res.status(400).send('ProductName must be different from his actual one.')
+        const result = await Product.findOneAndUpdate({_id: req.params.id},{$set: {productName:productName} }, {new: true})
+        //Updates orders containing that product
         const ordersUpdate = await Order.updateMany(
-            { "products.name": preUpdateProduct.name },
-            { $set: { "products.$[].name": req.body.name } },
-            { arrayFilters: [{ name: preUpdateProduct.name }] }
+            { "products.productName": preUpdateProduct.productName },
+            { $set: { "products.$[].productName": req.body.productName } },
+            { arrayFilters: [{ productName: preUpdateProduct.productName }] }
           ).exec().then((result)=>{
+            console.log(result)
             if (result.acknowledged == true){
-              ordersUpdatedString = `Product was included in ${result.matchedCount} order/s of which ${result.modifiedCount} have been updated`
+              ordersUpdatedString = `The Product was included in ${result.matchedCount} order/s of which ${result.modifiedCount} have been updated`
             }
           }
           );
+          
         console.log(result)
-        res.status(200).send('Product updated : '+JSON.stringify(result)+'<br/>'+ordersUpdatedString)
+        res.status(200).send('Product updated. '+ ordersUpdatedString)
             
         
     }catch (err){
